148018e40cc4272a819dc898267decc899fa19b3
David Andres Sanchez Umbarila <sanchezdavid481@gmail.com>
2024-01-21 20:25:28 -0500

    Reflect and headers

--- WS01/part2/event.h
+++ WS01/part2/event.h
@@ -1,3 +1,22 @@
+/***********************************************************************
+// Workshop 1 Part 2
+// Module: Event
+// File: event.h
+// Version 1.0
+// Author
+// Description
+//	Name      : David Andres Sanchez Umbarila
+//	Student ID: 140273228
+//	Email     : dasanchez-umbarila@myseneca.ca
+//
+//	I have done all the coding by myself and only copied the code
+//	 that my professor provided to complete my workshops and
+//	 assignments.
+// Revision History
+// -----------------------------------------------------------
+// Name                 Date            Reason
+***********************************************************************/
+
 #ifndef SENECA_EVENT_H
 #define SENECA_EVENT_H
 
