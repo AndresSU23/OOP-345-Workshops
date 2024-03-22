e3f28fafb2985dfb2642f8cb6ba1540a35eaeec3
David Andres Sanchez Umbarila <sanchezdavid481@gmail.com>
2024-03-22 16:54:04 -0400

    W8 submitted for Testing

---
+++ WS08/part2/utilities.cpp
@@ -0,0 +1,39 @@
+#include <memory>
+#include "list.h"
+#include "element.h"
+#include "utilities.h"
+
+using namespace std;
+
+namespace seneca {
+    List<Product> mergeRaw(const List<Description>& desc, const List<Price>& price) {
+        List<Product> priceList;
+
+        for (size_t i = 0; i < desc.size(); i++) {
+            for (size_t j = 0; j < price.size(); j++) {
+                if (desc[i].code == price[j].code) {
+                    Product mergedProduct{ desc[i].desc, price[j].price };
+                    mergedProduct.validate();
+                    priceList += (mergedProduct);
+                }
+            }
+        }
+
+        return priceList;
+    }
+
+	List<Product> mergeSmart(const List<Description>& desc, const List<Price>& price) {
+		List<Product> priceList;
+        for (size_t i = 0; i < desc.size(); i++) {
+            for (size_t j = 0; j < price.size(); j++) {
+                if (desc[i].code == price[j].code) {
+                    std::shared_ptr<Product> mergedProduct = std::make_shared<Product>(desc[i].desc, price[j].price);
+                    mergedProduct->validate();
+                    priceList += mergedProduct;
+                }
+            }
+        }
+
+		return priceList;
+	}
+}
